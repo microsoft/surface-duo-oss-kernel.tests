CondecoByEptura/kernel-tests

Test IPv4 transport mode on dual-stack sockets.
This is representative of what happens when standard Java Socket
objects, which are always AF_INET6 sockets, are connected to IPv4
addresses.

Bug: 64435904
Test: all_tests.sh passes on android-4.9
Change-Id: Ib6a52014e392a9372b03e4403fbc808b124cf737

net/test/xfrm_base.py
@@ -105,7 +105,11 @@ def ApplySocketPolicy(sock, family, direction, spi, reqid, tun_addrs):
 
   # Set the policy and template on our socket.
   opt_data = policy.Pack() + template.Pack()
-  SetSocketPolicyOption(sock, family, opt_data)
+
+  # The policy family might not match the socket family. For example, we might
+  # have an IPv4 policy on a dual-stack socket.
+  sockfamily = sock.getsockopt(SOL_SOCKET, net_test.SO_DOMAIN)
+  SetSocketPolicyOption(sock, sockfamily, opt_data)
 
 
 def GetEspPacketLength(mode, version, encap, payload):
@@ -139,6 +143,10 @@ def GetEspPacketLength(mode, version, encap, payload):
       },
       xfrm.XFRM_MODE_TRANSPORT: {
           None: {
+              # TODO: this is incorrect. It's likely looking at the ESP
+              # payload length instead of the total packet length. Fix it.
+              4: 84,
+              5: 84,
               6: 84,
           },
       },
@@ -177,7 +185,7 @@ class XfrmBaseTest(multinetwork_base.MultiNetworkBaseTest):
       netid: netid from which to read an ESP packet
       spi: SPI of the ESP packet
       seq: sequence number of the ESP packet
-      length: length of the packet's payload or None to skip this check
+      length: length of the packet's ESP payload or None to skip this check
       src_addr: source address of the packet or None to skip this check
       dst_addr: destination address of the packet or None to skip this check
 
@@ -188,11 +196,11 @@ class XfrmBaseTest(multinetwork_base.MultiNetworkBaseTest):
     self.assertEquals(1, len(packets))
     packet = packets[0]
     if length is not None:
-      self.assertEquals(length, len(packet.payload), "Incorrect packet length.")
+      self.assertEquals(length, len(packet.payload))
     if dst_addr is not None:
-      self.assertEquals(dst_addr, packet.dst, "Mismatched destination address.")
+      self.assertEquals(dst_addr, packet.dst)
     if src_addr is not None:
-      self.assertEquals(src_addr, packet.src, "Mismatched source address.")
+      self.assertEquals(src_addr, packet.src)
     # extract the ESP header
     esp_hdr, _ = cstruct.Read(str(packet.payload), xfrm.EspHdr)
     self.assertEquals(xfrm.EspHdr((spi, seq)), esp_hdr)

net/test/xfrm_test.py
@@ -104,22 +104,28 @@ class XfrmFunctionalTest(xfrm_base.XfrmBaseTest):
     self.xfrm.FlushSaInfo()
     self.assertEquals(0, len(self.xfrm.DumpSaInfo()))
 
-  def testSocketPolicy(self):
-    # Open an IPv6 UDP socket and connect it.
-    s = socket(AF_INET6, SOCK_DGRAM, 0)
+  def _TestSocketPolicy(self, version):
+    # Open a UDP socket and connect it.
+    family = net_test.GetAddressFamily(version)
+    s = socket(family, SOCK_DGRAM, 0)
     netid = self.RandomNetid()
     self.SelectInterface(s, netid, "mark")
-    s.connect((TEST_ADDR1, 53))
+
+    remoteaddr = self.GetRemoteAddress(version)
+    s.connect((remoteaddr, 53))
     saddr, sport = s.getsockname()[:2]
     daddr, dport = s.getpeername()[:2]
     reqid = 0
 
-    desc, pkt = packets.UDP(6, saddr, daddr, sport=sport)
-    s.sendto(net_test.UDP_PAYLOAD, (TEST_ADDR1, 53))
+    desc, pkt = packets.UDP(version, saddr, daddr, sport=sport)
+    s.sendto(net_test.UDP_PAYLOAD, (remoteaddr, 53))
     self.ExpectPacketOn(netid, "Send after socket, expected %s" % desc, pkt)
 
-
-    xfrm_base.ApplySocketPolicy(s, AF_INET6, xfrm.XFRM_POLICY_OUT,
+    # Using IPv4 XFRM on a dual-stack socket requires setting an AF_INET policy
+    # that's written in terms of IPv4 addresses.
+    xfrm_version = 4 if version == 5 else version
+    xfrm_family = net_test.GetAddressFamily(xfrm_version)
+    xfrm_base.ApplySocketPolicy(s, xfrm_family, xfrm.XFRM_POLICY_OUT,
                                 htonl(TEST_SPI), reqid, None)
 
     # Because the policy has level set to "require" (the default), attempting
@@ -127,59 +133,74 @@ class XfrmFunctionalTest(xfrm_base.XfrmBaseTest):
     # matches the socket policy we set.
     self.assertRaisesErrno(
         EAGAIN,
-        s.sendto, net_test.UDP_PAYLOAD, (TEST_ADDR1, 53))
+        s.sendto, net_test.UDP_PAYLOAD, (remoteaddr, 53))
 
     # Adding a matching SA causes the packet to go out encrypted. The SA's
     # SPI must match the one in our template, and the destination address must
     # match the packet's destination address (in tunnel mode, it has to match
     # the tunnel destination).
-    self.xfrm.AddMinimalSaInfo("::", TEST_ADDR1, htonl(TEST_SPI), IPPROTO_ESP,
+    self.xfrm.AddMinimalSaInfo(net_test.GetWildcardAddress(xfrm_version),
+                               self.GetRemoteAddress(xfrm_version),
+                               htonl(TEST_SPI), IPPROTO_ESP,
                                xfrm.XFRM_MODE_TRANSPORT, reqid,
                                xfrm_base._ALGO_CBC_AES_256,
                                xfrm_base._ENCRYPTION_KEY_256,
                                xfrm_base._ALGO_HMAC_SHA1,
                                xfrm_base._AUTHENTICATION_KEY_128,
                                None, None, None, None)
-    s.sendto(net_test.UDP_PAYLOAD, (TEST_ADDR1, 53))
-    expected_length = xfrm_base.GetEspPacketLength(xfrm.XFRM_MODE_TRANSPORT, 6,
-                                                   None, net_test.UDP_PAYLOAD)
+    s.sendto(net_test.UDP_PAYLOAD, (remoteaddr, 53))
+    expected_length = xfrm_base.GetEspPacketLength(xfrm.XFRM_MODE_TRANSPORT,
+                                                   version, None,
+                                                   net_test.UDP_PAYLOAD)
     self._ExpectEspPacketOn(netid, TEST_SPI, 1, expected_length, None, None)
 
     # Sending to another destination doesn't work: again, no matching SA.
+    remoteaddr2 = self.GetOtherRemoteAddress(version)
     self.assertRaisesErrno(
         EAGAIN,
-        s.sendto, net_test.UDP_PAYLOAD, (TEST_ADDR2, 53))
+        s.sendto, net_test.UDP_PAYLOAD, (remoteaddr2, 53))
 
     # Sending on another socket without the policy applied results in an
     # unencrypted packet going out.
-    s2 = socket(AF_INET6, SOCK_DGRAM, 0)
+    s2 = socket(family, SOCK_DGRAM, 0)
     self.SelectInterface(s2, netid, "mark")
-    s2.sendto(net_test.UDP_PAYLOAD, (TEST_ADDR1, 53))
+    s2.sendto(net_test.UDP_PAYLOAD, (remoteaddr, 53))
     pkts = self.ReadAllPacketsOn(netid)
     self.assertEquals(1, len(pkts))
     packet = pkts[0]
-    self.assertEquals(IPPROTO_UDP, packet.nh)
+
+    protocol = packet.nh if version == 6 else packet.proto
+    self.assertEquals(IPPROTO_UDP, protocol)
 
     # Deleting the SA causes the first socket to return errors again.
-    self.xfrm.DeleteSaInfo(TEST_ADDR1, htonl(TEST_SPI), IPPROTO_ESP)
+    self.xfrm.DeleteSaInfo(self.GetRemoteAddress(xfrm_version),
+                           htonl(TEST_SPI), IPPROTO_ESP)
     self.assertRaisesErrno(
         EAGAIN,
-        s.sendto, net_test.UDP_PAYLOAD, (TEST_ADDR1, 53))
+        s.sendto, net_test.UDP_PAYLOAD, (remoteaddr, 53))
 
     # Clear the socket policy and expect a cleartext packet.
-    xfrm_base.SetSocketPolicyOption(s, AF_INET6, None)
-    s.sendto(net_test.UDP_PAYLOAD, (TEST_ADDR1, 53))
+    xfrm_base.SetSocketPolicyOption(s, family, None)
+    s.sendto(net_test.UDP_PAYLOAD, (remoteaddr, 53))
     self.ExpectPacketOn(netid, "Send after clear, expected %s" % desc, pkt)
 
     # Clearing the policy twice is safe.
-    xfrm_base.SetSocketPolicyOption(s, AF_INET6, None)
-    s.sendto(net_test.UDP_PAYLOAD, (TEST_ADDR1, 53))
+    xfrm_base.SetSocketPolicyOption(s, family, None)
+    s.sendto(net_test.UDP_PAYLOAD, (remoteaddr, 53))
     self.ExpectPacketOn(netid, "Send after clear 2, expected %s" % desc, pkt)
 
     # Clearing if a policy was never set is safe.
     s = socket(AF_INET6, SOCK_DGRAM, 0)
-    xfrm_base.SetSocketPolicyOption(s, AF_INET6, None)
+    xfrm_base.SetSocketPolicyOption(s, family, None)
 
+  def testSocketPolicyIPv4(self):
+    self._TestSocketPolicy(4)
+
+  def testSocketPolicyIPv6(self):
+    self._TestSocketPolicy(6)
+
+  def testSocketPolicyMapped(self):
+    self._TestSocketPolicy(5)
 
   # TODO: Should we completely re-write this using null encryption and null
   # authentication? We could then assemble and disassemble packets for each

microsoft/surface-duo-oss-kernel.tests

net/test/xfrm_base.py
@@ -134,7 +134,11 @@ def ApplySocketPolicy(sock, family, direction, spi, reqid, tun_addrs):
 
   # Set the policy and template on our socket.
   opt_data = policy.Pack() + template.Pack()
-  SetPolicySockopt(sock, family, opt_data)
+
+  # The policy family might not match the socket family. For example, we might
+  # have an IPv4 policy on a dual-stack socket.
+  sockfamily = sock.getsockopt(SOL_SOCKET, net_test.SO_DOMAIN)
+  SetPolicySockopt(sock, sockfamily, opt_data)
 
 
 def GetEspPacketLength(mode, version, encap, payload):
@@ -168,6 +172,10 @@ def GetEspPacketLength(mode, version, encap, payload):
       },
       xfrm.XFRM_MODE_TRANSPORT: {
           None: {
+              # TODO: this is incorrect. It's likely looking at the ESP
+              # payload length instead of the total packet length. Fix it.
+              4: 84,
+              5: 84,
               6: 84,
           },
       },
@@ -297,7 +305,7 @@ class XfrmBaseTest(multinetwork_base.MultiNetworkBaseTest):
       netid: netid from which to read an ESP packet
       spi: SPI of the ESP packet in host byte order
       seq: sequence number of the ESP packet
-      length: length of the packet's payload or None to skip this check
+      length: length of the packet's ESP payload or None to skip this check
       src_addr: source address of the packet or None to skip this check
       dst_addr: destination address of the packet or None to skip this check
 
@@ -308,11 +316,11 @@ class XfrmBaseTest(multinetwork_base.MultiNetworkBaseTest):
     self.assertEquals(1, len(packets))
     packet = packets[0]
     if length is not None:
-      self.assertEquals(length, len(packet.payload), "Incorrect packet length.")
+      self.assertEquals(length, len(packet.payload))
     if dst_addr is not None:
-      self.assertEquals(dst_addr, packet.dst, "Mismatched destination address.")
+      self.assertEquals(dst_addr, packet.dst)
     if src_addr is not None:
-      self.assertEquals(src_addr, packet.src, "Mismatched source address.")
+      self.assertEquals(src_addr, packet.src)
     # extract the ESP header
     esp_hdr, _ = cstruct.Read(str(packet.payload), xfrm.EspHdr)
     self.assertEquals(xfrm.EspHdr((spi, seq)), esp_hdr)

net/test/xfrm_test.py
@@ -91,22 +91,28 @@ class XfrmFunctionalTest(xfrm_base.XfrmBaseTest):
     self.xfrm.FlushSaInfo()
     self.assertEquals(0, len(self.xfrm.DumpSaInfo()))
 
-  def testSocketPolicy(self):
-    # Open an IPv6 UDP socket and connect it.
-    s = socket(AF_INET6, SOCK_DGRAM, 0)
+  def _TestSocketPolicy(self, version):
+    # Open a UDP socket and connect it.
+    family = net_test.GetAddressFamily(version)
+    s = socket(family, SOCK_DGRAM, 0)
     netid = self.RandomNetid()
     self.SelectInterface(s, netid, "mark")
-    s.connect((TEST_ADDR1, 53))
+
+    remoteaddr = self.GetRemoteAddress(version)
+    s.connect((remoteaddr, 53))
     saddr, sport = s.getsockname()[:2]
     daddr, dport = s.getpeername()[:2]
     reqid = 0
 
-    desc, pkt = packets.UDP(6, saddr, daddr, sport=sport)
-    s.sendto(net_test.UDP_PAYLOAD, (TEST_ADDR1, 53))
+    desc, pkt = packets.UDP(version, saddr, daddr, sport=sport)
+    s.sendto(net_test.UDP_PAYLOAD, (remoteaddr, 53))
     self.ExpectPacketOn(netid, "Send after socket, expected %s" % desc, pkt)
 
-
-    xfrm_base.ApplySocketPolicy(s, AF_INET6, xfrm.XFRM_POLICY_OUT,
+    # Using IPv4 XFRM on a dual-stack socket requires setting an AF_INET policy
+    # that's written in terms of IPv4 addresses.
+    xfrm_version = 4 if version == 5 else version
+    xfrm_family = net_test.GetAddressFamily(xfrm_version)
+    xfrm_base.ApplySocketPolicy(s, xfrm_family, xfrm.XFRM_POLICY_OUT,
                                 TEST_SPI, reqid, None)
 
     # Because the policy has level set to "require" (the default), attempting
@@ -114,56 +120,72 @@ class XfrmFunctionalTest(xfrm_base.XfrmBaseTest):
     # matches the socket policy we set.
     self.assertRaisesErrno(
         EAGAIN,
-        s.sendto, net_test.UDP_PAYLOAD, (TEST_ADDR1, 53))
+        s.sendto, net_test.UDP_PAYLOAD, (remoteaddr, 53))
 
     # Adding a matching SA causes the packet to go out encrypted. The SA's
     # SPI must match the one in our template, and the destination address must
     # match the packet's destination address (in tunnel mode, it has to match
     # the tunnel destination).
-    self.xfrm.AddSaInfo("::", TEST_ADDR1, TEST_SPI,
-                        xfrm.XFRM_MODE_TRANSPORT, reqid,
+    self.xfrm.AddSaInfo(net_test.GetWildcardAddress(xfrm_version),
+                        self.GetRemoteAddress(xfrm_version),
+                        TEST_SPI, xfrm.XFRM_MODE_TRANSPORT, reqid,
                         xfrm_base._ALGO_CBC_AES_256, xfrm_base._ALGO_HMAC_SHA1,
                         None, None, None, None)
-    s.sendto(net_test.UDP_PAYLOAD, (TEST_ADDR1, 53))
+    s.sendto(net_test.UDP_PAYLOAD, (remoteaddr, 53))
     expected_length = xfrm_base.GetEspPacketLength(xfrm.XFRM_MODE_TRANSPORT, 6,
                                                    None, net_test.UDP_PAYLOAD)
+    expected_length = xfrm_base.GetEspPacketLength(xfrm.XFRM_MODE_TRANSPORT,
+                                                   version, None,
+                                                   net_test.UDP_PAYLOAD)
     self._ExpectEspPacketOn(netid, TEST_SPI, 1, expected_length, None, None)
 
     # Sending to another destination doesn't work: again, no matching SA.
+    remoteaddr2 = self.GetOtherRemoteAddress(version)
     self.assertRaisesErrno(
         EAGAIN,
-        s.sendto, net_test.UDP_PAYLOAD, (TEST_ADDR2, 53))
+        s.sendto, net_test.UDP_PAYLOAD, (remoteaddr2, 53))
 
     # Sending on another socket without the policy applied results in an
     # unencrypted packet going out.
-    s2 = socket(AF_INET6, SOCK_DGRAM, 0)
+    s2 = socket(family, SOCK_DGRAM, 0)
     self.SelectInterface(s2, netid, "mark")
-    s2.sendto(net_test.UDP_PAYLOAD, (TEST_ADDR1, 53))
+    s2.sendto(net_test.UDP_PAYLOAD, (remoteaddr, 53))
     pkts = self.ReadAllPacketsOn(netid)
     self.assertEquals(1, len(pkts))
     packet = pkts[0]
-    self.assertEquals(IPPROTO_UDP, packet.nh)
+
+    protocol = packet.nh if version == 6 else packet.proto
+    self.assertEquals(IPPROTO_UDP, protocol)
 
     # Deleting the SA causes the first socket to return errors again.
-    self.xfrm.DeleteSaInfo(TEST_ADDR1, TEST_SPI, IPPROTO_ESP)
+    self.xfrm.DeleteSaInfo(self.GetRemoteAddress(xfrm_version), TEST_SPI,
+                           IPPROTO_ESP)
     self.assertRaisesErrno(
         EAGAIN,
-        s.sendto, net_test.UDP_PAYLOAD, (TEST_ADDR1, 53))
+        s.sendto, net_test.UDP_PAYLOAD, (remoteaddr, 53))
 
     # Clear the socket policy and expect a cleartext packet.
-    xfrm_base.SetPolicySockopt(s, AF_INET6, None)
-    s.sendto(net_test.UDP_PAYLOAD, (TEST_ADDR1, 53))
+    xfrm_base.SetPolicySockopt(s, family, None)
+    s.sendto(net_test.UDP_PAYLOAD, (remoteaddr, 53))
     self.ExpectPacketOn(netid, "Send after clear, expected %s" % desc, pkt)
 
     # Clearing the policy twice is safe.
-    xfrm_base.SetPolicySockopt(s, AF_INET6, None)
-    s.sendto(net_test.UDP_PAYLOAD, (TEST_ADDR1, 53))
+    xfrm_base.SetPolicySockopt(s, family, None)
+    s.sendto(net_test.UDP_PAYLOAD, (remoteaddr, 53))
     self.ExpectPacketOn(netid, "Send after clear 2, expected %s" % desc, pkt)
 
     # Clearing if a policy was never set is safe.
     s = socket(AF_INET6, SOCK_DGRAM, 0)
-    xfrm_base.SetPolicySockopt(s, AF_INET6, None)
+    xfrm_base.SetPolicySockopt(s, family, None)
 
+  def testSocketPolicyIPv4(self):
+    self._TestSocketPolicy(4)
+
+  def testSocketPolicyIPv6(self):
+    self._TestSocketPolicy(6)
+
+  def testSocketPolicyMapped(self):
+    self._TestSocketPolicy(5)
 
   # TODO: Should we completely re-write this using null encryption and null
   # authentication? We could then assemble and disassemble packets for each